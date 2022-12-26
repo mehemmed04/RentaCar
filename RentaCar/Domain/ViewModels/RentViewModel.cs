5ac5a9597a96c0b3f50e7b9ab37a521f15d855f7
Bayr_kf60@itstep.edu.az <Bayr_kf60@itstep.edu.az>
2022-12-26 14:54:12 -0800

    Added RentWindow

---
+++ RentaCar/Domain/ViewModels/RentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentaCar.Domain.ViewModels
+{
+    internal class RentViewModel
+    {
+    }
+}
